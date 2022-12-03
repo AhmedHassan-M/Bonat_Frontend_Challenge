fd19466f70889d4f6ac5d92d52acbaf676a7df96
Ahmed Hassan <ahmahmedhassan@gmail.com>
2022-12-03 03:35:04 +0200

    install and try axios and bootstrap and setup environment variables

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.scss';
+import axios from 'axios';
+
+axios.get(`${process.env.REACT_APP_BACKENDURL}api/products`).then(res => console.log(res)).catch(err => console.log(err));
 
 function App() {
   return (
-    <div>
-      <h1>Test React is working!</h1>
+    <div className="container-fluid px-0">
+      <div className="container py-3">
+        <h1>Test React is working!</h1>
+      </div>
     </div>
   );
 }
